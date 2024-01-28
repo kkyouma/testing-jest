7399db6c6904bf32402f185ac1326057dea31600
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-01-27 23:50:20 -0300

    add analyze array function

--- src/js/analyze-array.js
+++ src/js/analyze-array.js
@@ -1,3 +1,23 @@
-const analizeArray = function (arr) {};
+const analyzeArray = function (arr) {
+  if (arr.length === 0) {
+    return { average: NaN, length: 0, min: undefined, max: undefined };
+  }
 
-module.exports = analizeArray;
+  let sum = 0;
+  let max = arr[0];
+  let min = arr[0];
+
+  arr.forEach(num => {
+    max = Math.max(num, max);
+    min = Math.min(num, min);
+
+    sum += num;
+    return { max, min };
+  });
+
+  const average = sum / arr.length;
+
+  return { average, min, max, length: arr.length };
+};
+
+module.exports = analyzeArray;
