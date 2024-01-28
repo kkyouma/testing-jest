c8d7202f09e277407c3f5f366e9a4f0433cd3c64
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-01-27 23:50:12 -0300

    fix test for analyzeArray

--- src/js/analyze-array.test.js
+++ src/js/analyze-array.test.js
@@ -3,19 +3,19 @@ const analyzeArray = require('./analyze-array');
 describe('analyzeArray', () => {
   test('positive numbers', () => {
     const result = analyzeArray([1, 8, 3, 4, 2, 6]);
-    expect(result).toBe({ average: 4, min: 1, max: 8, length: 6 });
+    expect(result).toEqual({ average: 4, min: 1, max: 8, length: 6 });
   });
   test('negative numbers', () => {
     const result = analyzeArray([-5, -2, -8, -1, -4]);
-    expect(result).toBe({ average: -4, min: -8, max: -1, length: 5 });
+    expect(result).toEqual({ average: -4, min: -8, max: -1, length: 5 });
   });
   test('decimals', () => {
     const result = analyzeArray([2.5, 1.8, 3.2, 4.7, 2.1]);
-    expect(result).toBe({ average: 2.66, min: 1.8, max: 4.7, length: 5 });
+    expect(result).toEqual({ average: 2.86, min: 1.8, max: 4.7, length: 5 });
   });
   test('empty', () => {
     const result = analyzeArray([]);
-    expect(result).toBe({
+    expect(result).toEqual({
       average: NaN,
       min: undefined,
       max: undefined,
@@ -24,6 +24,6 @@ describe('analyzeArray', () => {
   });
   test('just one element', () => {
     const result = analyzeArray([10]);
-    expect(result).toBe({ average: 10, min: 10, max: 10, length: 1 });
+    expect(result).toEqual({ average: 10, min: 10, max: 10, length: 1 });
   });
 });
